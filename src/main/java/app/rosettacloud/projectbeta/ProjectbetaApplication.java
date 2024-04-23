155333110ee976eebdfcf7f326d8c59dbcf8475c
mohamedsorour1998 <mohamedsorour1998@gmail.com>
2024-04-23 07:33:33 +0200

    first commit

---
+++ src/main/java/app/rosettacloud/projectbeta/ProjectbetaApplication.java
@@ -0,0 +1,13 @@
+package app.rosettacloud.projectbeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectbetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectbetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/rosettacloud/projectbeta/ProjectbetaApplicationTests.java
@@ -0,0 +1,13 @@
+package app.rosettacloud.projectbeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectbetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
